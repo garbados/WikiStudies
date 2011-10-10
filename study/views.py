7c3ae50fa812e1a1a769304822b76eac02fe6f89
Max Thayer <garbados@gmail.com>
2011-10-10 13:26:52 -0400

    Moar updates.

--- study/views.py
+++ study/views.py
@@ -1,1 +1,19 @@
 # Create your views here.
+from django.template import Context, loader
+from django.http import HttpResponse
+from study.models import *
+from django.http import Http404
+from django.shortcuts import render_to_response, get_object_or_404
+
+# study functions
+def studyIndex(request):
+    latest_study_list = Study.objects.all().order_by('id')[:5]
+    t = loader.get_template('studyIndex.html')
+    c = Context({
+        'latest_study_list': latest_study_list,
+    })
+    return HttpResponse(t.render(c))
+    
+def studyDetail(request, study_id):
+    s = get_object_or_404(Study, pk=study_id)
+    return render_to_response('studyDetail.html', {'study': s})
--- urls.py
+++ urls.py
@@ -7,10 +7,17 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Example:
     # (r'^wikistudies/', include('wikistudies.foo.urls')),
+    (r'^studies/$', 'study.views.studyIndex'),
+    (r'^studies/(?P<study_id>\d+)/$', 'study.views.studyDetail'),
+    #(r'^authors/$', 'study.views.authorIndex'),
+    #(r'^authors/(?P<author_id>\d+)/$', 'study.views.authorDetail'),
+    #(r'^users/$', 'study.views.userIndex'),
+    #(r'^users/(?P<user_id>\d+)/$', 'study.views.userDetail'),
+    
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    (r'^admin/', include(admin.site.urls)),
+    (r'^admin/', include(admin.site.urls))
 )
